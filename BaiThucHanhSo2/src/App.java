286cf37d5e7156c9caaf070f63d66f2415ec5915
nna20012000 <nna20012000@gmail.com>
2022-01-17 17:30:38 +0700

    BaiThucHanhSo2

---
+++ BaiThucHanhSo2/src/App.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        String strHoTen = "";
+        System.out.println("Nhap ho ten :");
+        strHoTen = sc.nextLine();
+        System.out.println("xin chao" + strHoTen);
+    }
+}
